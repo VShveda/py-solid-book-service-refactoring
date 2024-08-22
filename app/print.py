259ba01ee97e0353c2b2b015fbf54f71045eaf80
Viktor Sveda <vinix.ru@gmail.com>
2024-08-22 18:20:01 +0300

    Solution

---
+++ app/print.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+
+from app.book import Book
+
+
+class Print(ABC):
+    @abstractmethod
+    def print(self, book: "Book") -> None:
+        pass
+
+
+class ConsolePrint(Print):
+    def print(self, book: "Book") -> None:
+        print(f"Printing the book: {book.title}...")
+        print(book.content)
+
+
+class ReversePrint(Print):
+    def print(self, book: "Book") -> None:
+        print(f"Printing the book in reverse: {book.title}...")
+        print(book.content[::-1])
